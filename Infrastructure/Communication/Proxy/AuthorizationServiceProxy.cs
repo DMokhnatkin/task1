e59ac9d66ebe357a8437ffe23f2fe5439a04a44a
DMokhnatkin <dmitriy.mokhnatkin@gmail.com>
2016-11-29 23:18:06 +0300

    Added Authorization proxy

---
+++ Infrastructure/Communication/Proxy/AuthorizationServiceProxy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Communication.Service;
+
+namespace Infrastructure.Communication.Proxy
+{
+    public class AuthorizationServiceProxy : 
+        ClientBase<IAuthorizationService>,
+        IAuthorizationService
+    {
+        public void Login(string terminalId)
+        {
+            base.Channel.Login(terminalId);
+        }
+
+        public bool IsLogged(string terminalId)
+        {
+            return base.Channel.IsLogged(terminalId);
+        }
+    }
+}
